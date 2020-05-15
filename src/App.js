b16f8c404f836f7bd8be8338a4267219c4e7835f
kaly <kalimamadou@gmail.com>
2020-05-14 18:40:38 -0700

    start project fetch dad joke api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      weather: null,
+      joke: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://icanhazdadjoke.com/slack")
+      .then((res) => res.json())
+      .then((jokes) => {
+        let str = jokes.attachments[0].text.split("?");
+        this.setState({
+          joke: {
+            question: str[0],
+            answer: str[1],
+          },
+        });
+        console.log(this.state.joke);
+      });
+
+    // fetch("")
+    //   .then((response) => response.json())
+    //   .then((users) => this.setState({ }));
+  }
+  render() {
+    return <div className="App">Hello</div>;
+  }
 }
 
 export default App;
